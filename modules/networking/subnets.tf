44f98aa6f31319bcf2df91820885fc4ed464d3b3
Gautam Sethi <gautamsethigagan@gmail.com>
2025-10-15 23:50:11 +0530

    subnets.tf

---
+++ modules/networking/subnets.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true  # ✅ This ensures public instances get a public IP
+
+  tags = {
+    Name = "3-tier-PublicSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "3-tier-PrivateSubnet-${count.index + 1}"
+  }
+}
